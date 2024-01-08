fadc450be0214bdff20426da49583ebed3eacee8
Mahesh9944 <154601723+Mahesh9944@users.noreply.github.com>
2024-01-08 12:22:03 +0530

    Add files via upload

---
+++ INJECTOR HOOK.cpp
@@ -0,0 +1,118 @@
+
+
+    #include "livis.h"
+int hsub_38AB8C(){
+printf("Injector based hooking..");
+}
+
+int hsub_38787C(){
+printf("Injector based hooking..");
+}
+
+int hsub_6F6D2FC(){
+printf("Injector based hooking..");
+}
+
+int hsub_60C8DD8(){
+printf("Injector based hooking..");
+}
+
+int hsub_3855A0(){
+printf("Injector based hooking..");
+}
+
+int hsub_389D78(){
+printf("Injector based hooking..");
+}
+
+int hsub_15DDD0(){
+printf("Injector based hooking..");
+}
+
+int hsub_15AD44(){
+printf("Injector based hooking..");
+}
+
+int hsub_386D28(){
+printf("Injector based hooking..");
+}
+
+int hsub_34E55C(){
+printf("Injector based hooking..");
+}
+
+int main(){
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x38AB8C", (void *) hsub_38AB8C);
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x38787C", (void *) hsub_38787C);
+hook_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x6F6D2FC", (void *) hsub_6F6D2FC);
+hook_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x60C8DD8", (void *) hsub_60C8DD8);
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x3855A0", (void *) hsub_3855A0);
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x389D78", (void *) hsub_389D78);
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x15DDD0", (void *) hsub_15DDD0);
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x386D28", (void *) hsub_386D28);
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x34E55C", (void *) hsub_34E55C);
+}
+
+
+
+#include "livis.h"
+int hsub_38AB8C(){
+printf("Injector based hooking..");
+}
+
+int hsub_38787C(){
+printf("Injector based hooking..");
+}
+
+int hsub_34F070(){
+printf("Injector based hooking..");
+}
+
+int hsub_60C8DD8(){
+printf("Injector based hooking..");
+}
+
+int hsub_3855A0(){
+printf("Injector based hooking..");
+}
+
+int hsub_389D78(){
+printf("Injector based hooking..");
+}
+
+int hsub_15DDD0(){
+printf("Injector based hooking..");
+}
+
+int hsub_15AD44(){
+printf("Injector based hooking..");
+}
+
+int hsub_386D28(){
+printf("Injector based hooking..");
+}
+
+int hsub_34E55C(){
+printf("Injector based hooking..");
+}
+
+int main(){
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x38AB8C", (void *) hsub_38AB8C);
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x38787C", (void *) hsub_38787C);
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x34F070", (void *) hsub_34F070);
+hook_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x60C8DD8", (void *) hsub_60C8DD8);
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x3855A0", (void *) hsub_3855A0);
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x389D78", (void *) hsub_389D78);
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x15DDD0", (void *) hsub_15DDD0);
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x386D28", (void *) hsub_386D28);
+hook_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x34E55C", (void *) hsub_34E55C);
+}
+
+   
+ sub_34E460
+
+    
+
+
+
+
